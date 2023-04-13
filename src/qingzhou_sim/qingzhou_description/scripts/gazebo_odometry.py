cadb072a1a6237e31d57ac4829722ae479043e69
kenny <kenny0326@qq.com>
2023-04-13 21:44:17 +0800

    multi_descrip

--- src/qingzhou_sim/qingzhou_description/scripts/gazebo_odometry.py
+++ src/qingzhou_sim/qingzhou_description/scripts/gazebo_odometry.py
@@ -5,42 +5,49 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist, Transform, TransformStamped
-from gazebo_msgs.msg import LinkStates
+from gazebo_msgs.msg import ModelStates
 from std_msgs.msg import Header
 import numpy as np
 import math
-import tf2_ros
 
 class OdometryNode:
     # Set publishers
     pub_odom = rospy.Publisher('/odom', Odometry, queue_size=1)
+    pub_odom2 = rospy.Publisher('/odom2', Odometry, queue_size=1)
 
     def __init__(self):
         # init internals
         self.last_received_pose = Pose()
         self.last_received_twist = Twist()
+        self.last_received_pose2 = Pose()
+        self.last_received_twist2 = Twist()
         self.last_recieved_stamp = None
+        self.last_recieved_stamp2 = None
 
         # Set the update rate
         rospy.Timer(rospy.Duration(.05), self.timer_callback) # 20hz
 
-        self.tf_pub = tf2_ros.TransformBroadcaster()
-
         # Set subscribers
-        rospy.Subscriber('/qingzhou/gazebo/link_states', LinkStates, self.sub_robot_pose_update)
+        rospy.Subscriber('/gazebo/model_states', ModelStates, self.sub_robot_pose_update)
 
     def sub_robot_pose_update(self, msg):
         # Find the index of the racecar
         try:
-            arrayIndex = msg.name.index('qingzhou1::base_link')
+            arrayIndex = msg.name.index('/qingzhou_0')
+            arrayIndex2 = msg.name.index('/qingzhou_1')
         except ValueError as e:
             # Wait for Gazebo to startup
+            #print("1")
             pass
         else:
             # Extract our current position information
             self.last_received_pose = msg.pose[arrayIndex]
             self.last_received_twist = msg.twist[arrayIndex]
+            self.last_received_pose2 = msg.pose[arrayIndex2]
+            self.last_received_twist2 = msg.twist[arrayIndex2]
+            #print(self.last_received_twist2)
         self.last_recieved_stamp = rospy.Time.now()
+        self.last_recieved_stamp2 = rospy.Time.now()
 
     def timer_callback(self, event):
         if self.last_recieved_stamp is None:
@@ -48,39 +55,47 @@ class OdometryNode:
 
         cmd = Odometry()
         cmd.header.stamp = self.last_recieved_stamp
-        cmd.header.frame_id = 'qingzhou1/odom'
-        cmd.child_frame_id = 'qingzhou1/base_link'
+        cmd.header.frame_id = 'qingzhou_0/odom'
+        cmd.child_frame_id = 'qingzhou_0/base_link'
         cmd.pose.pose = self.last_received_pose
         cmd.twist.twist = self.last_received_twist
         cmd.pose.covariance =[1e-3, 0, 0, 0, 0, 0,
-						0, 1e-3, 0, 0, 0, 0,
-						0, 0, 1e6, 0, 0, 0,
-						0, 0, 0, 1e6, 0, 0,
-						0, 0, 0, 0, 1e6, 0,
-						0, 0, 0, 0, 0, 1e3]
+                              0, 1e-3, 0, 0, 0, 0,
+                              0, 0, 1e6, 0, 0, 0,
+                              0, 0, 0, 1e6, 0, 0,
+                              0, 0, 0, 0, 1e6, 0,
+                              0, 0, 0, 0, 0, 1e3]
 
-        cmd.twist.covariance = [1e-9, 0, 0, 0, 0, 0, 
-                          0, 1e-3, 1e-9, 0, 0, 0,
-                          0, 0, 1e6, 0, 0, 0,
-                          0, 0, 0, 1e6, 0, 0,
-                          0, 0, 0, 0, 1e6, 0,
-                          0, 0, 0, 0, 0, 1e-9]
+        cmd.twist.covariance = [1e-9, 0, 0, 0, 0, 0,
+                                0, 1e-3, 1e-9, 0, 0, 0,
+                                0, 0, 1e6, 0, 0, 0,
+                                0, 0, 0, 1e6, 0, 0,
+                                0, 0, 0, 0, 1e6, 0,
+                                0, 0, 0, 0, 0, 1e-9]
+        cmd2 = Odometry()
+        cmd2.header.stamp = self.last_recieved_stamp2
+        cmd2.header.frame_id = 'qingzhou_1/odom'
+        cmd2.child_frame_id = 'qingzhou_1/base_link'
+        cmd2.pose.pose = self.last_received_pose2
+        cmd2.twist.twist = self.last_received_twist2
+        cmd2.pose.covariance =[1e-3, 0, 0, 0, 0, 0,
+                              0, 1e-3, 0, 0, 0, 0,
+                              0, 0, 1e6, 0, 0, 0,
+                              0, 0, 0, 1e6, 0, 0,
+                              0, 0, 0, 0, 1e6, 0,
+                              0, 0, 0, 0, 0, 1e3]
+
+        cmd2.twist.covariance = [1e-9, 0, 0, 0, 0, 0,
+                                0, 1e-3, 1e-9, 0, 0, 0,
+                                0, 0, 1e6, 0, 0, 0,
+                                0, 0, 0, 1e6, 0, 0,
+                                0, 0, 0, 0, 1e6, 0,
+                                0, 0, 0, 0, 0, 1e-9]
 
 
+        self.pub_odom2.publish(cmd2)
         self.pub_odom.publish(cmd)
 
-        tf = TransformStamped(
-            header=Header(
-                frame_id=cmd.header.frame_id,
-                stamp=cmd.header.stamp
-            ),
-            child_frame_id=cmd.child_frame_id,
-            transform=Transform(
-                translation=cmd.pose.pose.position,
-                rotation=cmd.pose.pose.orientation
-            )
-        )
-        self.tf_pub.sendTransform(tf)
 
 # Start the node
 if __name__ == '__main__':
